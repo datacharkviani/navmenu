a40bbb5589a0869de78284c1533b98da6c19a1f3
datacharkviani <109875116+datacharkviani@users.noreply.github.com>
2022-12-13 23:32:13 +0400

    navbar

---
+++ app/src/main/java/com/example/navmenu/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.navmenu
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation)
+        navController = findNavController(R.id.NavHostFragment)
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
